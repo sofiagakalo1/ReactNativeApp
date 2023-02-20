6632a89cd6a3ec54e1c8c6629eff0dd8d3e0fbab
sofiagakalo1 <sofiagakalo1@gmail.com>
2023-02-20 16:46:05 +0100

    Add existing project files to Git

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hello! It's my firs project!</Text>
+      <Text>Hello! It's my firs project!!!</Text>
       <StatusBar style="auto" />
     </View>
   );
