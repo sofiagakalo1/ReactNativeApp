cb4125dbd6476731c31f3ec591d1da33570c02f4
sofiagakalo1 <sofiagakalo1@gmail.com>
2023-04-25 21:42:57 +0200

    1

---
+++ src/Screens/RegistrationScreen.jsx
@@ -0,0 +1,99 @@
+import React from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  ImageBackground,
+  TextInput,
+  Button,
+  TouchableOpacity,
+  Platform
+} from "react-native";
+
+const RegistrationScreen = () => {
+  console.log(Platform.OS)
+  return (
+    <View style={styles.container}>
+      <ImageBackground
+        source={require("../images/registrationbg.jpg")}
+        style={styles.image}
+      >
+        <View style={styles.form}>
+          <Text style={styles.headerText}>Регистрация</Text>
+          <View style={{ marginTop: 33 }}>
+            <TextInput style={styles.input} placeholder="Логин" />
+          </View>
+          <View style={{ marginTop: 16 }}>
+            <TextInput
+              style={styles.input}
+              placeholder="Адрес электронной почты"
+            />
+          </View>
+          <View style={{ marginTop: 16 }}>
+            <TextInput
+              style={styles.input}
+              placeholder="Пароль"
+              secureTextEntry={true}
+            />
+          </View>
+          <TouchableOpacity activeOpacity={0.8} style={styles.btn}>
+            <Text style={styles.btn_text}>Зарегистрироваться</Text>
+          </TouchableOpacity>
+        </View>
+      </ImageBackground>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  image: {
+    flex: 1,
+    resizeMode: "cover",
+    justifyContent: "flex-end",
+    // alignItems: "center",
+  },
+  form: {
+    flex: 0.5,
+    backgroundColor: "#ffffff",
+    borderTopLeftRadius: 25,
+    borderTopRightRadius: 25,
+  },
+  input: {
+    height: 50,
+    marginHorizontal: 16,
+    padding: 10,
+    backgroundColor: "#F6F6F6",
+    borderWidth: 1,
+    // borderStyle:solid,
+    borderColor: "#E8E8E8",
+    textAlign: "left",
+    color: "#BDBDBD",
+  },
+  headerText: {
+    fontStyle: "normal",
+    fontSize: 30,
+    lineHeight: 35,
+    textAlign: "center",
+    letterSpacing: 0.01,
+    color: "#212121",
+  },
+  btn: {
+    backgroundColor: "#FF6C00",
+    height: 51,
+    borderRadius: 100,
+    marginHorizontal: 16,
+    marginTop: 43,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  btn_text: {
+    color: "#FFFFFF",
+    fontSize: 16,
+    lineHeight: 19,
+  },
+});
+
+export default RegistrationScreen;
